038d73d425efd655f84eb57fa80f40d29b12fe27
julio cesar <juliocesar.pardo@vodafone.com>
2024-11-27 02:12:27 +0100

    fichero primero - septimo commit and push

--- primero.py
+++ primero.py
@@ -7,3 +7,5 @@ fichero primero-quinto commit and push
 fichero primero-quinto commit and push
 fichero primero-quinto commit and not push
 fichero primero-sexto commit and push
+
+fichero primero-septimo commit and push
